ef786d464797363c389b9bab989f1098f994dfc3
Edgar Muyodi <edgarmuyodi@gmail.com>
2024-07-09 13:23:00 +0000

    add all changes for index.html,style.css and setelctors in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 //select Elements
 let countSpan = document.querySelector('.count span');
+let flagImgDiv = document.querySelector('.flag-img');
+let flagImg = document.querySelector('.flag-img img');
+let flagOptions = document.querySelector('.flag-option ul');
+let flagLis = document.querySelector('.flag-option ul li');
+let score = document.querySelector('h3 span');
+let scoreDiv = document.querySelector('.score');
+let correctAns = document.querySelector('.score .right span');
+let incorrectAns = document.querySelector('.score .incorrect span');
+let btnNewGame = document.querySelector('#newGame');
+let currentIndex = 0;
+let rightAnswer = 0;
